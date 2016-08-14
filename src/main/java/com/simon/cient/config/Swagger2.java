76455ef72fd51695a46eca89f79d47f102fb6c3d
SimonSun <simon.sun.dev@hotmail.com>
2016-08-14 20:00:00 +0800

    修改了swagger2描述信息

--- src/main/java/com/simon/cient/config/Swagger2.java
+++ src/main/java/com/simon/cient/config/Swagger2.java
@@ -29,9 +29,9 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("程序猿DD")
+                .description("description")
+                .termsOfServiceUrl("termsOfServiceUrl")
+                .contact("contact")
                 .version("1.0").build();
     }
 }
