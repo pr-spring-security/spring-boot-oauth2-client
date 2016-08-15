55586eb320d02b90f562b165db80f9e7c015561f
SimonSun <simon.sun.dev@hotmail.com>
2016-08-16 04:00:25 +0800

    修改了数据源为mongodb+postgresql。原来为mongodb+mysql。并实现了JdbcTemplate操作

--- src/main/java/com/simon/cient/CientApplication.java
+++ src/main/java/com/simon/cient/CientApplication.java
@@ -1,14 +1,25 @@
 package com.simon.cient;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class CientApplication {
+public class CientApplication implements CommandLineRunner {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(CientApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		int rowCount = this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+		System.out.println(rowCount);
 	}
 }
